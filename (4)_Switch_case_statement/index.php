e3ad17fc09068a0ccdabd6aeaeb50e2f7d5e6c6c
satyajay01 <satyajaydibya0@gmail.com>
2024-05-08 23:04:05 +0530

    Switch case statement

---
+++ (4)_Switch_case_statement/index.php
@@ -0,0 +1,21 @@
+<!-- Switch case statement condition is used to identify whether the condition matches the case or not -->
+
+<?php
+$color = "green";
+
+switch ($color) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
+
+
+?>
