a2b3a0e2dc3b64bafd0f2273b3b49db4075fb66a
satyajay01 <satyajaydibya0@gmail.com>
2024-05-10 23:19:33 +0530

    Arrays

--- (7)_Arrays/index.php
+++ (7)_Arrays/index.php
@@ -2,12 +2,12 @@
 
 <?php
 //Create Arrays---------
-$myArray = [1,2,3, "satyajay", "dibya", "DOB"=>2001];
+$myArray = [1, 2, 3, "satyajay", "dibya", "DOB" => 2001];
 // $myArray = array (1,2,3, "satyajay", "dibya");  You can create an array using the array() function:
 
 //Access Arrays------------
-echo $myArray[2]."<br>";
-echo $myArray[3]." ".$myArray[4];
+echo $myArray[2] . "<br>";
+echo $myArray[3] . " " . $myArray[4];
 echo $myArray["DOB"];
 echo "<br><br>";
 
@@ -18,19 +18,19 @@ foreach ($myArray as $element) {
 echo "<br><br>";
 //Update Arrays-------------
 
-$myCar =["Volvo", "BMW", "Toyota", "Year"=>2023];
+$myCar = ["Volvo", "BMW", "Toyota", "Year" => 2023];
 
 $myCar[2] = "Rolls Royce";
-$myCar[ "Year"]=2024 ;
+$myCar["Year"] = 2024;
 
-echo $myCar[2]."<br>";
+echo $myCar[2] . "<br>";
 echo $myCar["Year"];
 
 // -------------------
 echo "<br>";
-$number =[2, 3, 4, 5];
+$number = [2, 3, 4, 5];
 foreach ($number as &$i) {
-  $i *= 2; 
+    $i *= 2;
 }
 
 echo $number[2];
@@ -38,9 +38,9 @@ echo $number[2];
 echo "<br><br>";
 //Add Array Item ---------------
 
-$fruits = ["Apple"=>123, "Banana", "Cherry"];
+$fruits = ["Apple" => 123, "Banana", "Cherry"];
 $fruits[] = "Orange";
-$fruits["Apple"]=456;
+$fruits["Apple"] = 456;
 
 foreach ($fruits as $element) {
     echo $element . " ";
@@ -59,7 +59,7 @@ foreach ($cars2 as $element) {
 echo "<br><br>";
 // Sort Arrays------------------
 
-$mynumber = [2,4,1,5,3,6];
+$mynumber = [2, 4, 1, 5, 3, 6];
 sort($mynumber);
 
 foreach ($mynumber as $element) {
@@ -75,20 +75,49 @@ echo "<br><br>";
 // Associative Array-----------------------
 
 $employees = array(
-    "Satyajay"=> "Software Engineer",
-    "Abhi"=> "Web Developer",
-    "Ravi"=> "App Developer",
+    "Satyajay" => "Software Engineer",
+    "Abhi" => "Web Developer",
+    "Ravi" => "App Developer",
 );
 
-?>
+$employees_key = array_keys($employees);
+print_r($employees_key);
+echo "<br>";
+
+$employees_value = array_keys($employees);
+print_r($employees_value);
+echo "<br>";
+
+$myemployees = array_reverse($employees);
+print_r($myemployees);
+echo "<br>";
 
 
-<?php
+foreach ($employees as $key => $value) {
+    echo "<br>";
+    echo $key . " ";
+    echo $value . " ";
+}
 
-foreach($employees as $key => $value){
-    ?>
-    <p> <?php echo $key ?></p> <p> <?php echo $value ?></p> 
-    <?php }
+
+
+
+echo "<br><br>";
+// Multidimensional Array------------
+
+$students = array(
+    array("Name" => "Satyajay", "Class" => "BCA"),
+    array("Name" => "jay", "Class" => "MCA")
+);
+
+foreach ($students as $key => $value) {
+
+    foreach ($value as $k => $v) {
+        echo "<b> $k </b> : ".$v ."<br>"; 
+    }
+}
+
+
 
 
 ?>
